f4632041ea88be749abf54095580a442d2b4c89c
nardo <129193276+nardomaciel@users.noreply.github.com>
2024-02-26 09:23:09 -0300

    Add files via upload

---
+++ ex001.js
@@ -0,0 +1,11 @@
+let nome ="leo"
+let idade = 17
+let escola = "eron domingues"
+let numIdade = 30
+let peso = 90
+let altura = 2
+soma = idade + numIdade
+imc = (altura*altura)/peso
+console.log(soma)
+console.log(imc)
+// console.log(`o ${nome} tem ${idade} anos, e estuda no ${escola}.`)
